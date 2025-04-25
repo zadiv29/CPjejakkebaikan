ef130f39fcad7b64f52c2d2792e76e01ad8ac0ed
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-04-25 23:36:56 +0700

    membuat migration untuk voluntrip

---
+++ database/migrations/2025_04_24_154804_create_voluntrip_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voluntrip', function (Blueprint $table) {
+            $table->id();
+            $table->string('thumbnail');
+            $table->string('name');
+            $table->unsignedBigInteger('target_amount');
+            $table->text('about');
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->boolean('is_active');
+            $table->foreignId('fundraiser_id')->constrained()->onDelete('cascade');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('ticket_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voluntrip');
+    }
+};
