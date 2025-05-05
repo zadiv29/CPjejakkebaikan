00982dcea21dd180ec138dfb278d4bf909bd8297
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-05 16:36:10 +0700

    menambahkan route voluntrip

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,30 @@ use App\Http\Controllers\FundraisingPhaseController;
 use App\Http\Controllers\FundraisingWithDrawalController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontController;
-
+use App\Http\Controllers\VoluntripController;
 
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
 
 Route::get('/category/{category}', [FrontController::class, 'category'])
-->name('front.category');
+    ->name('front.category');
 
 Route::get('/details/{fundraising:slug}', [FrontController::class, 'details'])
-->name('front.details');
+    ->name('front.details');
 
 Route::get('/support/{fundraising:slug}', [FrontController::class, 'support'])
-->name('front.support');
+    ->name('front.support');
 
 Route::get('/checkout/{fundraising:slug}/{totalAmountDonation}', [FrontController::class, 'checkout'])
-->name('front.checkout');
+    ->name('front.checkout');
 
 Route::post('/checkout/store/{fundraising:slug}/{totalAmountDonation}', [FrontController::class, 'store'])
-->name('front.store');
+    ->name('front.store');
+
+Route::get('/details/voluntrip/{voluntrip:slug}', [FrontController::class, 'voluntripDetail'])
+    ->name('front.voluntrip.details');
+
+Route::post('/buy/ticket/{voluntrips:slug}/{priceTicket}', [FrontController::class, 'buyTicket'])
+    ->name('front.voluntrip.store');
 
 
 Route::middleware('auth')->group(function () {
@@ -41,50 +47,50 @@ Route::middleware('auth')->group(function () {
     Route::prefix('admin')->name('admin.')->group(function () {
 
         Route::resource('categories', CategoryController::class)
-        ->middleware('role:owner');
+            ->middleware('role:owner');
 
         Route::resource('donaturs', DonaturController::class)
-        ->middleware('role:owner');
+            ->middleware('role:owner');
 
         Route::resource('fundraisers', FundraiserController::class)
-        ->middleware('role:owner')->except('index');
+            ->middleware('role:owner')->except('index');
 
         Route::get('fundraisers', [FundraiserController::class, 'index'])
-        ->name('fundraisers.index');
+            ->name('fundraisers.index');
 
         Route::resource('fundraising_withdrawals', FundraisingWithdrawalController::class)
-        ->middleware('role:owner|fundraiser');
+            ->middleware('role:owner|fundraiser');
 
         Route::post('/fundraising_withdrawals/request/{fundraising}', [FundraisingWithdrawalController::class, 'store'])
-        ->middleware('role:fundraiser')
-        ->name('fundraising_withdrawals.store');
+            ->middleware('role:fundraiser')
+            ->name('fundraising_withdrawals.store');
 
         Route::resource('fundraising_phases', FundraisingPhaseController::class)
-        ->middleware('role:owner|fundraiser');
+            ->middleware('role:owner|fundraiser');
 
-        Route::post('/fundraising_phases/update/{fundraising}',[FundraisingPhaseController::class, 'store'])
-        ->middleware('role:fundraiser')
-        ->name('fundraising_phases.store');
+        Route::post('/fundraising_phases/update/{fundraising}', [FundraisingPhaseController::class, 'store'])
+            ->middleware('role:fundraiser')
+            ->name('fundraising_phases.store');
 
         Route::resource('fundraisings', FundraisingController::class)
-        ->middleware('role:owner|fundraiser');
+            ->middleware('role:owner|fundraiser');
 
         Route::post('/fundraisings/active/{fundraising}', [FundraisingController::class, 'activate_fundraising'])
-        ->middleware('role:owner')
-        ->name('fundraising_withdrawals.activate_fundraising');
+            ->middleware('role:owner')
+            ->name('fundraising_withdrawals.activate_fundraising');
 
         Route::post('/fundraiser/apply', [DashboardController::class, 'apply_fundraiser'])
-        ->name('fundraiser.apply');
+            ->name('fundraiser.apply');
 
         Route::get('/my-withdrawals', [DashboardController::class, 'my_withdrawals'])
-        ->name('my-withdrawals');
+            ->name('my-withdrawals');
 
         Route::get('/my-withdrawals/details/{fundraisingWithdrawal}', [DashboardController::class, 'my_withdrawals_details'])
-        ->name('my-withdrawals.details');
+            ->name('my-withdrawals.details');
 
-        
-
+        Route::resource('/voluntrip', VoluntripController::class)
+            ->middleware('role:owner|fundraiser');
     });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
