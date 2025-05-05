87dc274bd03e49ba9f6b6b1655f02e7f9aafb6ed
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-05 16:31:13 +0700

    menambahkan controller untuk tampilan depan voluntrip

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,64 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreVoluntripRequest;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Fundraising;
 use App\Http\Requests\StoreDonationRequest;
 use Illuminate\Support\Facades\DB;
 use App\Models\Donatur;
-
+use App\Models\Voluntrip;
+use Carbon\Carbon;
 
 class FrontController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $categories = Category::all();
 
         $fundraisings = Fundraising::with(['category', 'fundraiser'])
-        ->where('is_active', 1)
-        ->orderByDesc('id')
-        ->get();
+            ->where('is_active', 1)
+            ->orderByDesc('id')
+            ->get();
 
-        return view('front.views.index', compact('categories', 'fundraisings'));
+        $voluntrips = Voluntrip::where('is_active', 1)->get();
+
+        return view('front.views.index', compact('categories', 'fundraisings', 'voluntrips'));
     }
 
-    public function category(Category $category){
+    public function category(Category $category)
+    {
         return view('front.views.category', compact('category'));
     }
 
-    public function details(Fundraising $fundraising){
-        $goalReached = $fundraising->totalReachedAmount()>= $fundraising->target_amount;
+    public function details(Fundraising $fundraising)
+    {
+        $goalReached = $fundraising->totalReachedAmount() >= $fundraising->target_amount;
         return view('front.views.details', compact('fundraising', 'goalReached'));
     }
 
-    public function support(Fundraising $fundraising){
+    public function support(Fundraising $fundraising)
+    {
         return view('front.views.donation', compact('fundraising'));
     }
 
-    public function checkout(Fundraising $fundraising, $totalAmountDonation){
+    public function checkout(Fundraising $fundraising, $totalAmountDonation)
+    {
         return view('front.views.checkout', compact('fundraising', 'totalAmountDonation'));
     }
 
-    public function store(StoreDonationRequest $request, Fundraising $fundraising, $totalAmountDonation){
+    public function store(StoreDonationRequest $request, Fundraising $fundraising, $totalAmountDonation)
+    {
         DB::transaction(function () use ($request, $fundraising, $totalAmountDonation) {
-    
+
             $validated = $request->validated();
-    
-            if($request->hasFile('proof')){
+
+            if ($request->hasFile('proof')) {
                 $proofPath = $request->file('proof')->store('proofs', 'public');
                 $validated['proof'] = $proofPath;
             }
-    
+
             $validated['fundraising_id'] = $fundraising->id;
             $validated['total_amount'] = $totalAmountDonation;
             $validated['is_paid'] = false;
-    
+
             $donatur = Donatur::create($validated);
         });
-    
+
         return redirect()->route('front.details', $fundraising->slug);
     }
-    
 
+    //SECTION - Voluntrip Section
+    // NOTE Chekout voluntrip
+
+    public function voluntripDetail(Request $request, Voluntrip $voluntrip)
+    {
+        $date = Carbon::parse($voluntrip->start_date)->format('d F Y');
+        $timeStart = Carbon::parse($voluntrip->start_time)->format('H.i');
+        $timeEnd = Carbon::parse($voluntrip->end_time)->format('H.i');
+
+        // Active tab
+        $tab = $request->query('tab', 'deskripsi');
+
+        return (view('front.views.detail-voluntrip',   compact('voluntrip', 'date', 'timeStart', 'timeEnd', 'tab')));
+    }
 }
