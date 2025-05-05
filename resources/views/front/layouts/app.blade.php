f35d821853dd84a18978b31441c61616151730b0
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-05 16:37:18 +0700

    mengubah app.blade layout untuk menggunakan vite

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,19 +1,20 @@
 <!doctype html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-    @stack('before-style')
-    <link href="{{asset('css/output.css')}}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{asset('assets/js/flickity.min.css')}}" media="screen">
-    @stack('after-style')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>@yield('title')</title>
 </head>
-<body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
+
+<body class="font-poppins bg-[#F6F9FC] text-[#292E4B]">
 
 
     @yield('content')
@@ -21,11 +22,13 @@
 
     @stack('before-script')
     <!-- JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-    <script src="{{asset('assets/js/flickity.pkgd.min.js')}}"></script>
+    <script src="{{ asset('assets/js/flickity.pkgd.min.js') }}"></script>
 
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
     @stack('after-script')
 </body>
+
 </html>
