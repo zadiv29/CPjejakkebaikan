ce164e7027a7003e3ae5c89c8f3da60917065359
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-07 16:10:26 +0700

    menambahkan component icon

---
+++ resources/views/components/icon/plus.blade.php
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+        color="currentColor">
+        <path d="M12 8v8m4-4H8" />
+        <circle cx="12" cy="12" r="10" />
+    </g>
+</svg>
---
+++ resources/views/components/icon/ticket.blade.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" class="rotate-90">
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+        d="M8 2.486V2.5a2 2 0 1 0 4 0l-.001-.015H14.5a1 1 0 0 1 1 1V16.5a1 1 0 0 1-1 1H12a2 2 0 1 0-4 0H5.5a1 1 0 0 1-1-1V3.485a1 1 0 0 1 1-1zM6.5 6.5h1m2 0h1m2 0h1m-7 7h1m2 0h1m2 0h1"
+        stroke-width="1" />
+</svg>
