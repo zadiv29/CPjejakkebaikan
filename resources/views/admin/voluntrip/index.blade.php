15b4bfad69162a14011fbdaa3f9df7051a1411eb
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-13 12:02:46 +0700

    memperbaiki layout tampilan card-item

--- resources/views/admin/voluntrip/index.blade.php
+++ resources/views/admin/voluntrip/index.blade.php
@@ -15,33 +15,34 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div
-                class="grid gap-x-5 overflow-hidden bg-white p-10 shadow-sm sm:rounded-lg md:grid-cols-2 lg:grid-cols-4">
+            <div class="grid gap-5 overflow-hidden bg-white p-10 shadow-sm sm:rounded-lg md:grid-cols-2 lg:grid-cols-4">
                 @forelse($voluntrips as $voluntrip)
-                    <div class="card-item flex max-h-fit flex-col overflow-hidden rounded-lg bg-white shadow-md">
+                    <div class="card-item flex max-h-[500px] flex-col overflow-hidden rounded-lg bg-white shadow-md">
                         <div class="thumbnail">
                             <img src="{{ Storage::url($voluntrip->thumbnail) }}" alt="thumbnail"
                                 class="h-[200px] w-full">
                         </div>
-                        <div class="card-body flex flex-col gap-2 px-3 pb-4 pt-3">
-                            <p class="fundraiser text-[12px] font-thin">
-                                {{ $voluntrip->fundraiser->user->hasRole('owner') ? 'By Owner' : $voluntrip->fundraiser->user->name }}
-                            </p>
-                            <h4 class="title-voluntrip font-bold">{{ $voluntrip->name }}</h4>
-                            <div class="donation-section flex flex-col gap-2">
-                                <div class="flex items-center gap-2">
-                                    <p class="text-[13px] font-thin">Funded</p>
-                                    <p class="text-[15px] font-medium">
-                                        Rp{{ number_format($voluntrip->total_price, 0, ',', '.') }}</p>
-                                </div>
-                                <div class="h-2.5 w-full rounded-full bg-slate-300">
-                                    <div class="h-2.5 rounded-full bg-indigo-600" style="width: 10%">
+                        <div class="card-body flex h-full flex-col justify-between gap-4 px-3 pb-4 pt-3">
+                            <div>
+                                <p class="fundraiser text-[12px] font-thin">
+                                    {{ $voluntrip->fundraiser->user->hasRole('owner') ? 'By Owner' : $voluntrip->fundraiser->user->name }}
+                                </p>
+                                <h4 class="title-voluntrip line-clamp-2 font-bold">{{ $voluntrip->name }}</h4>
+                                <div class="donation-section flex flex-col gap-2">
+                                    <div class="flex items-center gap-2">
+                                        <p class="text-[13px] font-thin">Funded</p>
+                                        <p class="text-[15px] font-medium">
+                                            Rp{{ number_format($voluntrip->total_price, 0, ',', '.') }}</p>
+                                    </div>
+                                    <div class="h-2.5 w-full rounded-full bg-slate-300">
+                                        <div class="h-2.5 rounded-full bg-indigo-600" style="width: 10%">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <a href="{{ route('admin.voluntrip.show', $voluntrip->slug) }}"
+                                class="rounded-md bg-indigo-700 py-1 text-center text-white">Lihat Detail</a>
                         </div>
-                        <a href="{{ route('admin.voluntrip.show', $voluntrip->slug) }}"
-                            class="bg-indigo-700 py-1 text-center text-white">Lihat Detail</a>
                     </div>
                 @empty
                     <p>
