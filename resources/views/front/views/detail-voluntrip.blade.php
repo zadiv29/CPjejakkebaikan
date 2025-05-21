53378271da17039370ae5cded0370d79520b796d
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-21 17:41:18 +0700

    membuat tampilan tiket di detail voluntrip

--- resources/views/front/views/detail-voluntrip.blade.php
+++ resources/views/front/views/detail-voluntrip.blade.php
@@ -22,6 +22,7 @@
                 <img src="{{ Storage::url($voluntrip->thumbnail) }}" class="h-full w-full object-cover" alt="cover">
             </div>
         </div>
+
         <div class="z-30 flex flex-col pb-[24px]">
             <div id="content" class="flex w-full flex-col gap-5 rounded-t-[40px] bg-white">
                 <div class="flex flex-col gap-4" x-data="{ tab: 'deskripsi' }">
@@ -36,6 +37,7 @@
                             <span class="flex items-center gap-2 text-[12px] font-normal"><x-icon.location /> Jalan Tluki,
                                 Condong Catur
                                 Yogyakarta</span>
+
                         </div>
                     </section>
                     <section class="flex w-full gap-3 border-b-4 border-t border-b-[#c4e5f850] px-[24px] py-3">
@@ -75,29 +77,63 @@
                                 <h4 class="text-md font-medium">Manfaat/Benefit Program</h4>
                             </div>
                             {{-- //NOTE belum ada data untuk manfaat di db  --}}
-                            <p class="text-[13px]">{{ $voluntrip->about }} Allah SWT berfirman: "Sesungguhnya orang-orang
-                                yang
-                                membenarkan (Allah dan Rasul-Nya) baik laki-laki maupun perempuan dan meminjamkan kepada
-                                Allah
-                                peinjaman yang baik, niscaya akan dilipat gandakan (pembayarannya) kepada mereka; dan bagi
-                                mereka pahala yang banyak." (QS. Al hadid: 18)</p>
+                            <p class="text-[13px]">{{ $voluntrip->about }}</p>
                         </section>
                     </section>
                     {{-- #ANCHOR Tiket --}}
-                    <section x-show="tab === 'tiket'" class="px-10">
-                        <div class="relative flex h-[150px] flex-col justify-between rounded-lg bg-[#b3b3b34a] px-4 py-3">
-                            <div class="flex flex-col">
-                                <p>#{{ $voluntrip->name }}a</p>
-                                <p>{{ $voluntrip->total_ticket }} tiket tersedia</p>
+                    <section x-data="{ qty: 1, max: {{ $voluntrip->total_ticket }}, price: {{ $voluntrip->ticket_price }} }" x-show="tab === 'tiket'"
+                        class="flex flex-col justify-between space-y-10 px-[24px] py-6">
+                        <div
+                            class="relative mx-auto flex h-auto w-full flex-col justify-between overflow-hidden rounded-xl shadow-lg">
+                            <!-- Bagian atas -->
+                            <div class="flex justify-between rounded-xl border border-b-0 border-gray-400 p-4">
+                                <div class="flex flex-col gap-3">
+                                    <h2 class="text-[14px] font-semibold text-black sm:text-base">
+                                        {{ $voluntrip->name }}a
+                                    </h2>
+                                    <div class="flex items-center gap-1">
+                                        <x-icon.ticket />
+                                        <p class="text-sm text-gray-700" x-text="max + ' Tiket'"></p>
+                                    </div>
+                                </div>
+                                <div class="-mr-3 h-6 w-6 rounded-full border border-gray-300 bg-white shadow-md"></div>
                             </div>
-                            <div class="garis w-full border border-dashed border-gray-500"></div>
-                            <div class="flex items-center justify-between">
-                                <p>Rp{{ number_format($voluntrip->ticket_price, 0, ',', '.') }}</p>
-                                <button class="rounded-md bg-[#0060AF] px-4 text-white">Beli</button>
+
+                            <!-- Garis putus-putus -->
+                            <div class="relative z-10">
+                                <div
+                                    class="absolute left-0 top-1/2 -ml-3 h-6 w-6 -translate-y-1/2 rounded-full border border-gray-400 bg-white shadow-md">
+                                </div>
+                                <hr class="border-1 border-t border-dashed border-black" />
+                                <div
+                                    class="absolute right-0 top-1/2 -mr-3 h-6 w-6 -translate-y-1/2 rounded-full border border-gray-400 bg-white shadow-md">
+                                </div>
                             </div>
+
+                            <!-- Bagian bawah -->
+                            <div
+                                class="relative z-0 flex items-center justify-between rounded-xl border border-t-0 border-gray-400 bg-white p-4">
+                                <p class="text-[14px] font-bold text-black sm:text-lg"
+                                    x-text="'Rp' + (qty * price).toLocaleString('id-ID')"></p>
+                                <div class="flex items-center">
+                                    <button @click="qty = Math.max(1, qty - 1)">
+                                        <x-icon.minus />
+                                    </button>
+                                    <input type="text" x-model="qty" class="w-14 border-none text-center" readonly
+                                        disabled />
+                                    <button @click="if(qty < max) qty++"><x-icon.plus /></button>
+                                </div>
+                            </div>
+
+                            <!-- Tombol Beli -->
                         </div>
+                        <a :href="`{{ route('volunteers.create', ['voluntrip' => $voluntrip->slug]) }}?qty=${qty}&total=${qty * price}`"
+                            class="block w-full rounded-md bg-[#0060AF] py-2 text-center text-white transition hover:bg-blue-700"
+                            x-text="'Beli ' + qty + ' Tiket'">
+                        </a>
                     </section>
                 </div>
             </div>
+        </div>
     </section>
 @endsection
