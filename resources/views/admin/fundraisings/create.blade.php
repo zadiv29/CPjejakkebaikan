5d2365fe00bab112a7a186ac9dd8731ef64225fa
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-22 18:16:22 +0700

    membuat separator untuk raget amoun

--- resources/views/admin/fundraisings/create.blade.php
+++ resources/views/admin/fundraisings/create.blade.php
@@ -1,50 +1,54 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('New Fundraising') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-3xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white p-10 shadow-sm sm:rounded-lg">
 
-                @if($errors->any())
-                    @foreach($errors->all() as $error)
-                        <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
-                            {{$error}}
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <div class="w-full rounded-3xl bg-red-500 py-3 text-white">
+                            {{ $error }}
                         </div>
                     @endforeach
                 @endif
-                
-                <form method="POST" action="{{route('admin.fundraisings.store')}}" enctype="multipart/form-data">
+
+                <form method="POST" action="{{ route('admin.fundraisings.store') }}" enctype="multipart/form-data">
                     @csrf
 
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input id="name" class="mt-1 block w-full" type="text" name="name"
+                            :value="old('name')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="thumbnail" :value="__('thumbnail')" />
-                        <x-text-input id="thumbnail" class="block mt-1 w-full" type="file" name="thumbnail" required autofocus autocomplete="thumbnail" />
+                        <x-text-input id="thumbnail" class="mt-1 block w-full" type="file" name="thumbnail" required
+                            autofocus autocomplete="thumbnail" />
                         <x-input-error :messages="$errors->get('thumbnail')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="target_amount" :value="__('target_amount')" />
-                        <x-text-input id="target_amount" class="block mt-1 w-full" type="number" name="target_amount" :value="old('target_amount')" required autofocus autocomplete="target_amount" />
+                        <x-text-input id="target_amount" class="mt-1 block w-full" type="text" name="target_amount"
+                            :value="old('target_amount')" required autofocus autocomplete="target_amount" />
                         <x-input-error :messages="$errors->get('target_amount')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="category" :value="__('category')" />
-                        
-                        <select name="category_id" id="category_id" class="py-3 rounded-lg pl-3 w-full border border-slate-300">
+
+                        <select name="category_id" id="category_id"
+                            class="w-full rounded-lg border border-slate-300 py-3 pl-3">
                             <option value="">Choose category</option>
-                            @foreach($categories as $category)
-                            <option value="{{$category->id}}">{{$category->name}}</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
                         </select>
 
@@ -53,13 +57,13 @@
 
                     <div class="mt-4">
                         <x-input-label for="about" :value="__('about')" />
-                        <textarea name="about" id="about" cols="30" rows="5" class="border border-slate-300 rounded-xl w-full"></textarea>
+                        <textarea name="about" id="about" cols="30" rows="5" class="w-full rounded-xl border border-slate-300"></textarea>
                         <x-input-error :messages="$errors->get('about')" class="mt-2" />
                     </div>
 
-                    <div class="flex items-center justify-end mt-4">
-            
-                        <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                    <div class="mt-4 flex items-center justify-end">
+
+                        <button type="submit" class="rounded-full bg-indigo-700 px-6 py-4 font-bold text-white">
                             Add New Fundraising
                         </button>
                     </div>
@@ -69,3 +73,12 @@
         </div>
     </div>
 </x-app-layout>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const input = document.getElementById('target_amount');
+        input.addEventListener('input', function() {
+            let value = input.value.replace(/\D/g, '');
+            input.value = new Intl.NumberFormat('id-ID').format(value);
+        });
+    });
+</script>
