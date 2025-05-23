d7a33c753971d8074b04babf5747627646ced2c2
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-23 23:52:15 +0700

    menggunakan vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,8 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="/build/assets/app-BIdKQ6AZ.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
@@ -29,7 +28,6 @@
             {{ $slot }}
         </div>
     </div>
-    <script src="/build/assets/app-Bo-u61x1.js"></script>
 </body>
 
 </html>
