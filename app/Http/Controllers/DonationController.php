b2d14c7856636455ce02c28c0df4c4acaff904f6
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-31 01:45:23 +0700

    membuat donation controller

---
+++ app/Http/Controllers/DonationController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDonationRequest;
+use App\Mail\DonationVerificationMail;
+use App\Models\Donatur;
+use App\Models\Fundraising;
+use App\Models\Payment;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+
+class DonationController extends Controller
+{
+    public function store(StoreDonationRequest $request, Fundraising $fundraising)
+    {
+        DB::transaction(function () use ($request, $fundraising) {
+
+            $validated = $request->validated();
+
+            $paymentChannel = $validated['payment_channel'];
+            $mailDonatur = $request->email;
+            $validated['fundraising_id'] = $fundraising->id;
+            $validated['is_paid'] = false;
+            $validated['verify_token'] = Str::random(60);
+            $amount = $request->amount;
+
+            $donatur = Donatur::create($validated);
+
+            Mail::to($mailDonatur)->send(new DonationVerificationMail($donatur, $paymentChannel, $fundraising, $amount));
+        });
+
+        return redirect()->route('front.details', $fundraising->slug);
+    }
+
+    public function verifyDonation($token, $paymentChannel, $amount)
+    {
+        $donatur = Donatur::where('verify_token', $token)->firstOrFail();
+
+        if ($donatur->payment) {
+            if ($donatur->payment->status === 'paid') {
+                return redirect()->route('payment.donation.already_verified', [
+                    'payment' => $donatur->payment->uuid
+                ]);
+            }
+            if ($donatur->payment->status === 'expired') {
+                return redirect()->route('payment.donation.already_expired', [
+                    'payment' => $donatur->payment->uuid
+                ]);
+            }
+
+            return redirect()->route('payment.donation.information', [
+                'payment' => $donatur->payment->uuid
+            ]);
+        }
+
+        $response = Http::withToken(env('OMMOPAY_API_TOKEN'))
+            ->post(
+                'https://api.ommopay.id/v1/virtual_account',
+                [
+                    'merchant_trx_id' => 'DN_' . time(),
+                    'payment_channel' => $paymentChannel,
+                    'amount' => $amount,
+                    'description' => `Pembayaran untuk program donasi`,
+                    'expired_time' => now()->addMinutes(value: 1)->toIso8601String(),
+                    'callback_url' => route('payment.donation.callback'),
+                ]
+            );
+
+        if ($response->successful()) {
+            $dataDonation = $response->json();
+            $data = $dataDonation['data'];
+
+            $expiredAt = Carbon::parse($data['expired_time'])->format('Y-m-d H:i:s');
+
+            $payment = Payment::create([
+                'uuid' => $data['uuid'],
+                'merchant_trx_id' => $data['merchant_trx_id'],
+                'amount' => $data['amount'],
+                'payment_method' => $data['payment_method'],
+                'payment_channel' => $data['payment_channel'],
+                'va_number' => $data['va_number'],
+                'expired_at' => $expiredAt,
+                'status' => 'pending',
+                'payment_type' => 'donasi'
+            ]);
+
+            $update = $donatur->update(['donation_payments_id' => $payment->id]);
+
+            return redirect()->route('payment.donation.information', ['payment' => $data['uuid']]);
+        }
+
+        return back()->withErrors('Gagal membuat Virtual Account. Silakan coba lagi.');
+    }
+
+    public function information(Payment $payment)
+    {
+        $donatur = $payment->donaturs;
+        $fundraising = $donatur->fundraising;
+
+        return view('front.views.donation.payment-donation-information', [
+            'donatur' => $donatur,
+            'payment' => $payment,
+            'fundraising' => $fundraising
+        ]);
+    }
+
+    public function expiredPage(Payment $payment)
+    {
+        $donatur = $payment->donaturs;
+        $fundraising = $donatur->fundraising;
+
+        return view('front.views.donation.already-expired', [
+            'donatur' => $donatur,
+            'payment' => $payment,
+            'fundraising' => $fundraising
+        ]);
+    }
+
+    public function alreadyPaid(Payment $payment)
+    {
+        $donatur = $payment->donaturs;
+        $fundraising = $donatur->fundraising;
+        return view('front.views.donation.already-paid', [
+            'donatur' => $donatur,
+            'payment' => $payment,
+            'fundraising' => $fundraising
+        ]);
+    }
+
+    public function paymentCallback(Request $request)
+    {
+
+        $uuid = $request->input('uuid');
+        $status = $request->input('status');
+
+        $payment = Payment::where('uuid', $uuid);
+
+        if ($payment) {
+            $payment->update([
+                'status' => $status,
+            ]);
+        }
+        return response()->json(['message' => 'Callback received']);
+    }
+}
