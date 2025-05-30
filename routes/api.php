32e71cb9ee824ccd9d8c123323c310c145042486
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-31 01:50:51 +0700

    menambahkan api untuk get callback donasi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DonationController;
 use App\Http\Controllers\FrontController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/payment/callback', [FrontController::class, 'paymentCallback'])
     ->name('payment.callback');
+
+Route::post('/payment/donation/callback', [DonationController::class, 'paymentCallback'])
+    ->name('payment.donation.callback');
