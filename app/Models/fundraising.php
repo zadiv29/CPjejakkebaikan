eede6d57208291a222edc794d442639065beba4f
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-05-31 13:59:22 +0700

    menambahkan function confirmedPayment untuk menghitung total persentase antar sesama relasi fundraising

--- app/Models/fundraising.php
+++ app/Models/fundraising.php
@@ -23,22 +23,40 @@ class Fundraising extends Model
         'target_amount',
     ];
 
-//orm
-    public function category(){
+    //orm
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function fundraiser(){
+    public function fundraiser()
+    {
         return $this->belongsTo(Fundraiser::class);
     }
 
-    public function donaturs(){
+    public function donaturs()
+    {
         return $this->hasMany(Donatur::class)->where('is_paid', 1);
+    }
+
+    // Ini adalah relasi kunci untuk perhitungan Anda
+    public function confirmedPayments()
+    {
+        return $this->hasManyThrough(
+            Payment::class,
+            Donatur::class,
+            'fundraising_id',
+            'donatur_id',
+            'id',
+            'id'
+        )
+            ->where('payments.status', 'paid')
+            ->where('donaturs.is_paid', true);
     }
 
     public function totalReachedAmount()
     {
-        return $this->donaturs()->sum('total_amount');
+        return $this->confirmedPayments()->sum('amount');
     }
 
     public function withdrawals()
@@ -52,16 +70,13 @@ class Fundraising extends Model
     }
 
 
-    public function getPercentageAttribute(){
+    public function getPercentageAttribute()
+    {
         $totalDonations = $this->totalReachedAmount();
-        if($this->target_amount > 0){
+        if ($this->target_amount > 0) {
             $percentage = ($totalDonations / $this->target_amount) * 100;
             return $percentage > 100 ? 100 : $percentage;
         }
         return 0;
     }
-    
-    
 }
-
-
