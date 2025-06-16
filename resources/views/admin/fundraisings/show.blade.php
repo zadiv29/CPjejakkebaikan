de02dd507037a869302ca4faa322c5ecfc616201
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-06-16 09:52:36 +0700

    update relasi get data amount

--- resources/views/admin/fundraisings/show.blade.php
+++ resources/views/admin/fundraisings/show.blade.php
@@ -162,7 +162,7 @@
                         <div class="flex flex-row items-center gap-x-3">
                             <div class="flex flex-col">
                                 <h3 class="text-xl font-bold text-indigo-950">Rp
-                                    {{ number_format($donatur->total_amount, 0, ',', '.') }}</h3>
+                                    {{ number_format($donatur->payment->amount, 0, ',', '.') }}</h3>
                                 <p class="text-sm text-slate-500">{{ $donatur->name }}</p>
                             </div>
                         </div>
