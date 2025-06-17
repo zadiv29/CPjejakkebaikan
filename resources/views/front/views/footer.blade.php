f86cef864e956f103b137d2c2b46003ee155f63b
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-06-18 00:40:09 +0700

    menambahkan footer

---
+++ resources/views/front/views/footer.blade.php
@@ -0,0 +1,53 @@
+<section class="mt-8 grid grid-cols-2 justify-center gap-3 bg-[#1F2937] p-[20px] text-white sm:grid-cols-4">
+    <div class="col-span-2 flex flex-col gap-2">
+        <h1 class="text-lg font-bold">Jejak Kebaikan</h1>
+        <p class="text-[#9CA3AF]">Platform donasi online terpercaya di Indonesia yang menghubungkan donatur dengan
+            orang-orang yang
+            membutuhkan.</p>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-lg font-bold">Tautan Cepat</h1>
+        <ul class="flex flex-col gap-2 text-[#9CA3AF]">
+            <li>
+                <a href="#">Beranda</a>
+            </li>
+            <li>
+                <a href="#">Program</a>
+            </li>
+            <li>
+                <a href="#">Tentang Kami</a>
+            </li>
+            <li>
+                <a href="#">Kontak</a>
+            </li>
+        </ul>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-lg font-bold">Legal</h1>
+        <ul class="flex flex-col gap-2 text-[#9CA3AF]">
+            <li>
+                <a href="#">Syarat & Ketentuan</a>
+            </li>
+            <li>
+                <a href="#">Kebijakan Privasi</a>
+            </li>
+            <li>
+                <a href="#">FAQ</a>
+            </li>
+        </ul>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-lg font-bold">Hubungi Kami</h1>
+        <ul class="flex flex-col gap-2 text-[#9CA3AF]">
+            <li>
+                <p>hello@jejakkebaikan.id</p>
+            </li>
+            <li>
+                <p>(021) 1234-5678</p>
+            </li>
+            <div>
+
+            </div>
+        </ul>
+    </div>
+</section>
