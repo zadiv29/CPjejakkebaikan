b97373a50a22a0bfd386a660859082a4e8df8e58
zadiv29 <rezadivaalfiansyah@gmail.com>
2025-06-18 00:38:16 +0700

    menghilangkan icon menu dot dan menyesuaikan tampilan beberapa category

--- resources/views/front/views/category.blade.php
+++ resources/views/front/views/category.blade.php
@@ -1,19 +1,16 @@
 @extends('front.layouts.app')
 @section('title', 'Category Donasi')
 @section('content')
-    <section class="mx-auto flex min-h-screen w-full max-w-[640px] flex-col overflow-x-hidden bg-[#FCF7F1] pb-4">
+    <section class="mx-auto flex min-h-screen w-full max-w-[640px] flex-col space-y-2 overflow-x-hidden bg-white pb-4">
         <div
-            class="header -mb-[181px] flex h-[320px] flex-col overflow-hidden rounded-b-[50px] bg-[#56BBC5] bg-gradient-to-b from-[#3CBBDB] to-[#EAD380]">
-            <nav class="flex items-center justify-between px-3 pt-5">
-                <div class="flex items-center gap-[10px]">
+            class="header flex h-fit flex-col overflow-hidden rounded-b-[50px] bg-[#56BBC5] bg-gradient-to-b from-[#3CBBDB] to-[#EAD380] py-10">
+            <nav class="relative flex items-center justify-between px-3 pt-5">
+                <div class="absolute flex items-center gap-[10px]">
                     <a href="{{ route('front.index') }}" class="flex h-10 w-10 shrink-0">
                         <img src="{{ asset('assets/images/icons/back.svg') }}" alt="icon">
                     </a>
                 </div>
-                <p class="text-sm font-semibold text-white">Ayo Berdonasi</p>
-                <a href="" class="flex h-10 w-10 shrink-0">
-                    <img src="{{ asset('assets/images/icons/menu-dot.svg') }}" alt="icon">
-                </a>
+                <p class="w-full text-center text-sm font-semibold text-white">Ayo Berdonasi</p>
             </nav>
             <div class="mt-5">
                 <h1 class="text-center text-[26px] font-bold leading-[39px] text-white">{{ $category->name }}</h1>
@@ -22,7 +19,7 @@
         <div class="flex flex-col gap-4 px-4">
 
             @forelse($category->activeFundraisings as $fundraising)
-                <a href="{{ route('front.details', $fundraising) }}" class="card">
+                <a href="{{ route('front.details', $fundraising) }}" class="card rounded-2xl border border-gray-300">
                     <div class="flex w-full items-center gap-3 rounded-2xl bg-white p-[14px]">
                         <div class="flex h-[90px] w-20 shrink-0 overflow-hidden rounded-2xl">
                             <img src="{{ Storage::url($fundraising->thumbnail) }}" class="h-full w-full object-cover"
